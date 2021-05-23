93cf1b046d3129ec5fe10b45d6127092cabc752c
bbosmedia <bbosmedia@users.noreply.github.com>
2021-05-23 18:46:46 +0500

    I finished my task

--- lib/main.dart
+++ lib/main.dart
@@ -5,109 +5,149 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: _MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class _MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  __MyHomePageState createState() => __MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class __MyHomePageState extends State<_MyHomePage> {
+  int photoIndex = 0;
 
-  void _incrementCounter() {
+  void _previousImage() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      photoIndex = photoIndex > 0 ? photoIndex - 1 : photos.length - 1;
     });
   }
 
+  void _nextImage() {
+    setState(() {
+      photoIndex = photoIndex < photos.length - 1 ? photoIndex + 1 : 0;
+    });
+  }
+
+  List<String> photos = [
+    'assets/images/burger1.jpg',
+    'assets/images/burger2.jpg',
+    'assets/images/burger3.jpg',
+    'assets/images/burger4.jpg'
+  ];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Stack(children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(photos[photoIndex]), fit: BoxFit.cover),
+              ),
+              height: 400.0,
+              width: double.infinity,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+            Positioned(
+              top: 375,
+              left: 25.0,
+              right: 25.0,
+              child: SelectedPhoto(numberOfDots: photos.length, photoIndex: photoIndex,),
+            )
+          ]),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: [
+              RaisedButton(
+                onPressed: _previousImage,
+                child: Text("Prev"),
+                elevation: 5.0,
+                color: Colors.green[500],
+              ),
+              RaisedButton(
+                  onPressed: _nextImage,
+                  child: Text("Next"),
+                  elevation: 5.0,
+                  color: Colors.red[500]),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class SelectedPhoto extends StatelessWidget {
+  final int numberOfDots;
+  final int photoIndex;
+  SelectedPhoto({required this.numberOfDots, required this.photoIndex});
+
+  Widget _inactivePhoto(){
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.only(left: 3.0, right: 3.0),
+        child: Container(
+          height: 8.0,
+          width: 8.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            color: Colors.grey,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                spreadRadius: 0.0,
+                blurRadius: 2.0
+              )
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  Widget _activePhoto(){
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.only(left: 3.0, right: 3.0),
+        child: Container(
+          height: 10.0,
+          width: 10.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5.0),
+            color: Colors.white,
+
+          ),
+        ),
+      ),
+    );
+  }
+
+  List <Widget> _buildDots(){
+    List<Widget> dots =[];
+
+    for(var i = 0; i < numberOfDots; i++){
+      dots.add(i == photoIndex ? _activePhoto() : _inactivePhoto());
+    }
+    return dots;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: _buildDots(),
+      ),
+    );
+}}
